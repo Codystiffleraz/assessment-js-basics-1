c8d5447eead1cb29e556b70c6d8661dad95d5580
unknown <codystiffleraz@gmail.com>
2022-08-19 09:04:39 -0700

    sdlc complete fr this time

--- sdlc.js
+++ sdlc.js
@@ -19,8 +19,8 @@ Maintain
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const Analysis of Requirements = "The process of determining user expectations for a new or modified product."
-const Design ="The process of envisioning and defining software solutions to one or more sets of problems"
-const Implementation ="Writing the actual code of the system that is being created"
-const Testing & Integration ="A type of software testing in which the different unit, modules or components of a software application are tested as a combinded entity."
+const analysisOfRequirements = "The process of determining user expectations for a new or modified product."
+const design ="The process of envisioning and defining software solutions to one or more sets of problems"
+const implementation ="Writing the actual code of the system that is being created"
+const testingIntegration ="A type of software testing in which the different unit, modules or components of a software application are tested as a combinded entity."
 const Maintain ="The process of changing, modifying and updating software."
